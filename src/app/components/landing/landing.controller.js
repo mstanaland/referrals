1f14c0974860ef1ad8e31434e636daf6acd534ce
Michael Stanaland <stanaland_michael@bah.com>
2016-12-21 05:34:17 -0500

    Added landing controller logic

--- src/app/components/landing/landing.controller.js
+++ src/app/components/landing/landing.controller.js
@@ -1,5 +1,18 @@
-function LandingController() {
-  console.log('landing controller!');
+function LandingController($state) {
+  this.onSubmit = function() {
+    switch(this.radio) {
+      case 'nc':
+        // $state.go('nc');
+        console.log('Non-compliance');
+        break;
+      case 'victim':
+        $state.go('victim');
+        break;
+      case 'comms':
+        $state.go('comms');
+        break;
+    }
+  }
 }
 
 angular
