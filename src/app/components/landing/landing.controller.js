8cf63e38b3cabdc38aad14acfa24b0b41ffa5250
Michael Stanaland <stanaland_michael@bah.com>
2016-12-23 13:20:29 -0600

    cleanup

--- src/app/components/landing/landing.controller.js
+++ src/app/components/landing/landing.controller.js
@@ -1,13 +1,22 @@
-function LandingController($state) {
+function LandingController($scope, $state, store) {
+
+  // Check if form was previously submited, if so set radio to checked
+  if ( store.values.landing ) {
+    this.radio = store.values.landing;
+  }
+
   this.onSubmit = function() {
     switch(this.radio) {
       case 'nc':
+        store.values.landing = 'nc';
         $state.go('nc.prescreen');
         break;
       case 'victim':
+        store.values.landing = 'victim';
         $state.go('victim');
         break;
       case 'comms':
+        store.values.landing = 'comms';
         $state.go('comms');
         break;
     }
