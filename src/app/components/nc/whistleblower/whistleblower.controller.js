77a4e137a070c7e682a897676e111320802763d7
Michael Stanaland <stanaland_michael@bah.com>
2016-12-23 13:18:56 -0600

    adds routing

--- src/app/components/nc/whistleblower/whistleblower.controller.js
+++ src/app/components/nc/whistleblower/whistleblower.controller.js
@@ -1,16 +1,25 @@
 function WhistleblowerController($state, store) {
   store.setProgressBar(0.1);
 
+  // Check if form was previously submited, if so set radio to checked
+  if ( store.values.nc.whistleblower ) {
+    this.radio = store.values.nc.whistleblower;
+  }
+
   this.back = function() {
     $state.go('nc.prescreen');
   };
 
   this.onSubmit = function() {
     switch(this.radio) {
-      case 'taxprep':
-        $state.go('taxprep');
+      case 'yes':
+        store.values.nc.whistleblower = 'yes';
+        // $state.go('whistleblower');
         break;
-
+      case 'no':
+        store.values.nc.whistleblower = 'no';
+        $state.go('nc.suspectType');
+        break;
     }
   };
 }
