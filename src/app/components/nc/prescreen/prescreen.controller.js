b65e529002445b741a3b07645d7b369b7916f08d
Michael Stanaland <stanaland_michael@bah.com>
2016-12-23 13:19:57 -0600

    routing adjustments

--- src/app/components/nc/prescreen/prescreen.controller.js
+++ src/app/components/nc/prescreen/prescreen.controller.js
@@ -1,6 +1,11 @@
 function ncPrescreenController($state, store) {
   store.setProgressBar(0.05);
 
+  // Check if form was previously submited, if so set radio to checked
+  if ( store.values.nc.prescreen ) {
+    this.radio = store.values.nc.prescreen;
+  }
+
   this.back = function() {
     $state.go('landing');
   };
@@ -8,15 +13,19 @@ function ncPrescreenController($state, store) {
   this.onSubmit = function() {
     switch(this.radio) {
       case 'taxprep':
+        store.values.nc.prescreen = 'taxprep';
         $state.go('taxprep');
         break;
       case 'refund':
+        store.values.nc.prescreen = 'refund';
         $state.go('refund');
         break;
       case 'spouse':
+        store.values.nc.prescreen = 'spouse';
         $state.go('spouse');
         break;
       case 'none':
+        store.values.nc.prescreen = 'none';
         $state.go('nc.whistleblower');
         break;
     }
